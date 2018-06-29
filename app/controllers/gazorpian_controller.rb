75f6202b211a1cc8cd4548e409c730528b1ba213
ho-tonym <ho.tonym@gmail.com>
2018-06-29 14:01:38 -0400

    stub out controller routes

--- app/controllers/gazorpian_controller.rb
+++ app/controllers/gazorpian_controller.rb
@@ -1,3 +1,77 @@
 class GazorpianController < Sinatra::Base
 
+  get '/gazorpians' do
+    if logged_in?
+      @gazorpians = Gazorpian.all
+      erb :'/gazorpians/gazorpians'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/gazorpians/new' do
+    if logged_in?
+      erb :'gazorpians/create_gazorpian'
+    else
+      redirect 'login'
+    end
+  end
+
+  post '/gazorpians' do
+    @gazorpian = current_user.gazorpians.build(content: params[:content])
+    if params[:content] == "" || !@gazorpian.save
+      redirect '/gazorpians/new'
+    else
+      redirect "/gazorpians/#{@gazorpian.id}"
+    end
+  end
+
+  get '/gazorpians/:id' do
+    if logged_in?
+      @gazorpian = Gazorpian.find_by_id(params[:id])
+      erb :'gazorpians/show_gazorpian'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/gazorpians/:id/edit' do
+    if logged_in?
+      @gazorpian = Gazorpian.find(params[:id])
+      if @gazorpian && @gazorpian.user == current_user
+        erb :'gazorpians/edit_gazorpian'
+      else
+        redirect '/gazorpians'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/gazorpians/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/gazorpians/#{params[:id]}/edit"
+      else
+        @gazorpian = Gazorpian.find_by_id(params[:id])
+        if @gazorpian && @gazorpian.user == current_user
+          if @gazorpian.update(content: params[:content])
+            redirect to "/gazorpians/#{@gazorpian.id}"
+          end
+        end
+      end
+    end
+  end
+
+  delete '/gazorpians/:id/delete' do
+    if logged_in?
+      @gazorpian = Gazorpian.find_by_id(params[:id])
+      if @gazorpian && @gazorpian.user == current_user
+        @gazorpian.delete
+        redirect "/gazorpians"
+      else
+        redirect "/login"
+      end
+    end
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,52 @@
 class UserController < Sinatra::Base
 
+  get '/signup' do
+    if logged_in?
+      redirect '/gazorpians'
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], password: params[:password], email: params[:email])
+      session[:user_id] = @user.id
+      redirect '/gazorpians'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+       redirect '/gazorpians'
+    else
+       erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/gazorpians'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 end
